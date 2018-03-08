56feac96a2b99de24e18dfd471a6f75b09cca220
Christian Fernau <cfernau@sonatype.com>
2018-03-08 14:56:07 +0700

    added one dependency

--- src/main/java/Mini.java
+++ src/main/java/Mini.java
@@ -1,2 +1,9 @@
+import java.util.LinkedList;
+
+import org.apache.commons.collections.ListUtils;
+
 class Mini {
+  public void Mini() {
+    ListUtils.fixedSizeList(new LinkedList<String>());
+  }
 }
